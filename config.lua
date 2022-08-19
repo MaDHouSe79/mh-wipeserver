96dd1bf830ee3dcc01d1ce701b16a5b14c51d575
MaDHouSe79 <madhouse1979@gmail.com>
2022-08-19 22:48:24 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 Config.WipeAfterSecs  = 60000 -- 1 min
 Config.ShowTimeNotify = 5000  -- 6 secs
-Config.WipeAllPlayers = true  -- true will do a complte server wipe, with false, all player wil remain untouched. 
+Config.WipeAllPlayers = true  -- true will do a complete server wipe, with false, all player will stay remain untouched. 
 
 
 -- These are the only tables that remain untouched if you have the `Config.WipeAllPlayers` set to false.
