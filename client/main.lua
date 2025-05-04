ecb164d3c27eadf3b2eeaaf00bd850282a6884a3
MaDHouSe <madhouse1979@gmail.com>
2025-05-04 11:30:11 +0200

    Delete client directory

--- client/main.lua
+++
@@ -1,9 +0,0 @@
---[[ ===================================================== ]]--
---[[         QBCore Server Wipe Script by MaDHouSe         ]]--
---[[ ===================================================== ]]--
-
-local QBCore = exports['qb-core']:GetCoreObject()
-
-RegisterNetEvent("qb-wipeserver:client:createNewChar", function(source)
-    TriggerEvent('qb-multicharacter:client:chooseChar')
-end)
